e4fc9cd3734d59095c1672a894eca63f5e0d4818
Jonas Dech <j.dech@gmx.net>
2021-10-04 09:44:08 +0200

    new werbsite

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ from std_msgs.msg import String
 import rospy
 
 app = Flask(__name__)
-node = rospy.init_node('pepper_app')
-pub = rospy.Publisher('/assistant', String, queue_size=10)
+#node = rospy.init_node('pepper_app')
+#pub = rospy.Publisher('/assistant', String, queue_size=10)
 
 @app.route("/")
 def start():
-    return render_template("index.html")
+    return render_template("index_new.html")
 
 @app.route("/<product>", methods=['GET', 'POST'])
 def send_product(product):
+    print(f"product: {product}")
     pub.publish(String(product))
     return render_template("index.html")
