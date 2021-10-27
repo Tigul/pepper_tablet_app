e49addd486509263afb6717df03a9916582b43a9
Jonas Dech <j.dech@gmx.net>
2021-10-27 12:54:09 +0200

    .

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from std_msgs.msg import String
 import rospy
 
 app = Flask(__name__)
-#node = rospy.init_node('pepper_app')
-#pub = rospy.Publisher('/assistant', String, queue_size=10)
+node = rospy.init_node('pepper_app')
+pub = rospy.Publisher('/assistant', String, queue_size=10)
 
 @app.route("/")
 def start():
     return render_template("index_new.html")
 
-@app.route("/<product>", methods=['GET', 'POST'])
+@app.route("/<product>", methods=['POST'])
 def send_product(product):
-    print(f"product: {product}")
-    pub.publish(String(product))
-    return render_template("index.html")
+    req = dict(request.form)
+    req['product'] = product
+    print(req)
+    msg = str(req).replace("'", '"')
+    print(msg)
+    pub.publish(String(msg))
+    return render_template("index_new.html")
