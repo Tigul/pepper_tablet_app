ca89a40b04fd546bff32324bea9b37813058c19c
Jonas Dech <j.dech@gmx.net>
2022-09-07 17:16:57 +0200

    added new grid style interface

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, render_template, request
 from std_msgs.msg import String
+from pepper_behaviour_srvs.srv import Visualize
 import rospy
 
 app = Flask(__name__)
-node = rospy.init_node('pepper_app')
-pub = rospy.Publisher('/assistant', String, queue_size=10)
+#node = rospy.init_node('pepper_app')
+#pub = rospy.Publisher('/assistant', String, queue_size=10)
+#vis_service = rospy.ServiceProxy('/visualize_shelf', Visualize)
 
 @app.route("/")
 def start():
-    return render_template("index_new.html")
+    return render_template("index_grid.html")
 
 @app.route("/<product>", methods=['POST'])
 def send_product(product):
     req = dict(request.form)
     req['product'] = product
-    print(req)
+    #print(req)
     msg = str(req).replace("'", '"')
     print(msg)
     pub.publish(String(msg))
-    return render_template("index_new.html")
+    vis_service(String(msg))
+    return render_template("vis_img.html")
